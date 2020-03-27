bdbcd271e6f08072b3b7691696b2a5377f7e9b59
mox-cmder <62528480+mox-cmder@users.noreply.github.com>
2020-03-27 22:34:02 +0200

    mox

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# mox12
+# Me Codes
+Code Welcome 
+By: AboRoh3942
+
+
+Discord : https://discord.gg/AEruVwB
---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    TOKEN: 'NjkxNjU5ODY1MTE3OTUwMDYz.XnjMjA.KI6QhWfuB7imVRZGOU5Wk42Wp5M',
+    prefix:  '#',
+    devs: ['507477981065773056']
+}
